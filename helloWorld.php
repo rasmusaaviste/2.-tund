04de276c6f3ea5f71264cd1de56ef2c97556ce09
rasmaavi <rasmaavi@greeny.cs.tlu.ee>
2016-09-14 09:35:23 +0300

    added some lines

--- helloWorld.php
+++ helloWorld.php
@@ -7,10 +7,41 @@
 	//echo $lastName;
 	
 	echo $firstName." ".$lastName;
+
+?>
+
+<br>
+
+<?php
+
+	$age = 18;
+	if ($age < 18) {
+		
+		// kui vastus on tõene (true)
+		echo "alaealine";
+	}else  {
 	
+		// kui vastus on väär (false)
+		echo "täisealine";
+	}
+?>
+	<br>
 	
+<?php
 
+	for ($i=0; $i < $age; $i=$i +1) {
+		
+		echo $i. "palju";
+		
+	}
+	
+	echo "õnne";
 
+?>
 
+<br>
 
+<?php
+
+	echo date("l d. F Y G:i:s");
 ?>
