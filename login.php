15602120cc9cd77afa24499abddbc3331eb3d62c
rasmaavi <rasmaavi@greeny.cs.tlu.ee>
2016-09-14 11:01:06 +0300

    added some more lines

---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump($_POST);
+	
+	$signupEmailerror = "";
+
+	//kas e-post oli olemas
+	if (isset ($_POST["signupEmail"])) {
+		
+		if (empty ($_POST ["signupEmail"])) {
+			//oli e-post, kuid see oli tühi
+			$signupEmailerror = "See väli on kohustuslik!";
+		}	
+	}
+	
+	$signupPassworderror = "";
+
+	//kas parool oli olemas
+	if (isset ($_POST["signupPassword"])) {
+		
+		if (empty ($_POST ["signupPassword"])) {
+			//oli parool, kuid see oli tühi
+			$signupPassworderror = "See väli on kohustuslik!";
+			
+			
+		}	else {
+			
+			//tean et parool ja see ei olnud tühi
+			//vähemalt 8
+			
+			if (strlen($_POST["signupPassword"]) < 8 ) {
+				$signupPassworderror = "Parool peab olema vähemalt 8 tähemärki pikk"; 
+			}
+		}
+	}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sisselogimise lehekülg</title>
+	</head>
+	<body>
+
+		<h1>Logi sisse</h1>
+		
+		<form method="POST">
+		
+			<input name="loginEmail" type="email" placeholder="E-post"> 
+			<br><br>
+			<input name="loginPassword" type="password" placeholder="Parool">
+			<br><br>
+			<input type="submit" value="Logi sisse">
+		
+		
+		</form>
+		
+		<head>
+		<title>Sisselogimise lehekülg</title>
+	</head>
+	<body>
+
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+		
+			<input name="signupEmail" type="email" placeholder="E-post">  <?php echo $signupEmailerror;  ?>
+			<br><br>
+			<input name="signupPassword" type="password" placeholder="Parool"> <?php echo $signupPassworderror; ?>
+			<br><br>
+			<input type="submit" value="Loo kasutaja">
+		
+		
+		</form>
+
+	</body>
+</html>
