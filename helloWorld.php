dafe682ccd2d11f84bf44d153d7fbb0fed33a013
rasmaavi <rasmaavi@greeny.cs.tlu.ee>
2016-09-14 08:54:53 +0300

    first code in line

---
+++ helloWorld.php
@@ -0,0 +1,16 @@
+<?php
+	$firstName="Rasmus";
+	$lastName="Aaviste";
+
+	//echo trükib välja muutuja väärtuse
+    //echo $firstName;
+	//echo $lastName;
+	
+	echo $firstName." ".$lastName;
+	
+	
+
+
+
+
+?>
